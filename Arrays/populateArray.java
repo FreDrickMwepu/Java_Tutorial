c0335ee88a3d80c39dc2d98aa6bb32c358c5e35b
fredrickmwepu <fredrickmwepu22@gmail.com>
2026-01-28 21:14:10 +0200

    Refactor populateArray class to streamline array initialization and input prompts

--- Arrays/populateArray.java
+++ Arrays/populateArray.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class populateArray {
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
-          String [] names;
-          names = new String[10];
+          String [] names = new String[10];
 
-          System.out.print("Enter Names: ");
-          int position = sc.nextInt();
-          names[0] = "Fredrick";
 
           for (int i = 0; i<=names.length; i++) {
-               System.out.print("Enter Name " + i);
+               System.out.print("Enter Name " + (i+1) + ": ");
+               names[i] = sc.nextLine();
           }
 
-
-
+          System.out.println("\nNames in the array are: ");
+          for (int i = 0; i<names.length; i++) {
+               System.out.println(names[i]);
+          }
      }
 }
